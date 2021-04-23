f624ee0a83807cae89b7ac261e93632f87f8c08c
Bjørn Harbo Dahle <bdahle@hey.com>
2021-04-23 10:33:45 +0200

    buttons log numbers

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+let displayValue = 0;
+
+const numberButtons = document.querySelectorAll(".numberButtons button");
+numberButtons.forEach((button) => {
+  button.addEventListener("click", displayNumber);
+});
+
+function displayNumber(e) {
+  console.log(e.target.textContent);
+}
+
 function add(a, b) {
   return a + b;
 }
