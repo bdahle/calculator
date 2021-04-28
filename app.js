c0e4e1e5a7c956678b3617d7c024461a0bff8dcf
Bjørn Harbo Dahle <bdahle@hey.com>
2021-04-28 17:20:35 +0200

    number buttons work with display

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 let displayValue = 0;
 
-const numberButtons = document.querySelectorAll(".number-buttons button");
-numberButtons.forEach((button) => {
-  button.addEventListener("click", displayNumber);
-});
+const display = document.querySelector(".display");
 
-function displayNumber(e) {
-  displayValue = Number(e.target.textContent);
-  document.getElementById("displayText").innerText = displayValue;
-}
+const keys = document.querySelector(".keys");
+keys.addEventListener("click", (e) => {
+  if (e.target.matches("button")) {
+    const key = e.target;
+    const action = key.dataset.action;
+    const displayValue = display.textContent;
+    if (!action) {
+      const keyNumber = key.textContent;
+      if (displayValue === "0") {
+        display.textContent = keyNumber;
+      } else {
+        display.textContent = displayValue + keyNumber;
+      }
+    } else if (action === "add") {
+    } else if (action === "subtract") {
+    } else if (action === "multiply") {
+    } else if (action === "divide") {
+    } else if (action === "equals") {
+    }
+  }
+});
 
 function add(a, b) {
   return a + b;
